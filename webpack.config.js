3e19a8e170d47fbb21643e91438fbe45a335ef74
Flarp <flurpdadurp@gmail.com>
2016-12-28 13:47:26 -0500

    Remove webpack configuration to embed fonts, as it's now fetched dynamically

--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,8 @@ module.exports = {
           presets: ['es2015', 'stage-0'],
           plugins: ['transform-object-assign']
         } },
-            {test: /\.js$/, loader: 'webpack-append', query: 'var _crypto;'},
+            { test: /\.js$/, loader: 'webpack-append', query: 'var _crypto;' },
             { test: /\.css$/, loader: 'style-loader!css-loader' },
-            { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
-            { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
-            { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
-            { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
     ]
   },
   plugins: [
